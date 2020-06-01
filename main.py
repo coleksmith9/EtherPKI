54e4db1cb9144ac595f6c4401b955e098f37220f
kyrios9 <colesmith559@gmail.com>
2020-06-01 17:33:52 -0500

    Added the basics of the command line utility, beginning to understand how the Click package works for Python.

---
+++ main.py
@@ -0,0 +1,18 @@
+import click
+
+@click.group()
+def main():
+    pass
+
+@click.command()
+@click.option("--attributetype", prompt="Attribute Type", type=str)
+@click.option("--identifier", prompt="Attribute Identifier", type=str)
+@click.option("--data", prompt="Data", help="Data", type=str)
+def add(attributetype, identifier, data):
+    """ADDs attributes to the network"""
+    click.echo("Adding! %s %s %s" % (attributetype, identifier, data))
+
+main.add_command(add)
+
+if __name__ == "__main__":
+    main()
