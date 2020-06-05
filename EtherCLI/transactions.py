6c3d8a8f30c93276243bd2d78a334fb7fbd76798
Cole Smith <colesmith559@gmail.com>
2020-06-05 05:40:35 -0500

    added more functions to transactions.py, created gpgapi.py for later use.

--- EtherCLI/transactions.py
+++ EtherCLI/transactions.py
@@ -7,14 +7,15 @@ from ethapi import ETHERPKI_ABI
 from ethapi import ETHERPKI_DEFAULT_ADDRESS
 from ethapi import ethclient
 from ethapi import encode_api_data
+import ipfshttpclient
 
 
 class Transactions(object):
     def __init__(self, from_address=None, to_address=ETHERPKI_DEFAULT_ADDRESS):
         """Initialize transactions.
 
-        from_address: the Ethereum address transactions should come from
-        to_address: the Ethereum EtherPKI contract address.
+        from_address:   the Ethereum address transactions should come from
+        to_address:     the Ethereum EtherPKI contract address.
         """
         if from_address is None:
             # Uses the first Ethereum account address if none is specified.
@@ -26,3 +27,86 @@ class Transactions(object):
 
         # initialize contract ABI
         self._contracttranslator = abi.ContractTranslator(ETHERPKI_ABI)
+
+    def _send_transaction(self, data):
+        """ Sends a transaction to the Ethereum client.
+
+        data:   the data to be sent.
+        """
+        return ethclient.send_transaction(
+            _from=self.from_address,
+            to=self.to_address,
+            data=encode_api_data(data),
+            gas=100 # TODO: modify?
+        )
+
+    def add_attribute(self, attributetype, has_proof, identifier, data, datahash):
+        """Sends a transaction to add an attribute.
+
+        attributetype:  the type of attribute.
+        has_proof:      True if the attribute has proof, otherwise false.
+        identifier:     the indexable identifier of the attribute.
+        data:           the data of the attribute.
+        datahash:       the hash of the data if it is stored off of the blockchain.
+        """
+        args = [attributetype, has_proof, identifier, data, datahash]
+        data = self._contracttranslator.encode('addAttribute', args)
+        return self._send_transaction(data)
+
+    def add_attribute_with_hash(self, attributetype, has_proof, identifier, data):
+        """Sends a transaction to add an attribute, but it calculates the datahash automatically.
+
+        attributetype:  the type of attribute.
+        has_proof:      True if the attribute has proof, otherwise false.
+        identifier:     the indexable identifier of the attribute.
+        data:           the data of the attribute.
+        """
+        datahash = '' # TODO: calculate hash
+        return self.add_attribute(attributetype, has_proof, identifier, data, datahash)
+
+    def add_attribute_over_ipfs(self, attributetype, has_proof, identifier, data):
+        """Sends a transaction to add an attribute & stores the data on IPFS.
+
+        attributetype:  the type of attribute.
+        has_proof:      True if the attribute has proof, otherwise false.
+        identifier:     the indexable identifier of the attribute.
+        data:           the data of the attribute.
+        """
+
+        # stores the data as an IPFS block and gets the key.
+        with ipfshttpclient.connect() as client:
+            ipfs_key = client.block.put(io.StringIO(data))['Key']
+
+        # generates the EtherPKI-specific URI for the IPFS block
+        ipfs_uri = 'ipfs-block://' + ipfs_key
+
+        # adds the attribute by automatically calculating the hash
+        self.add_attribute_with_hash(attributetype, has_proof, identifier, ipfs_uri)
+
+    def add_pgp_attribute_over_ipfs(self, keyid):
+        """Send a transaction to add an identity PGP attribute, storing the data on IPFS.
+
+        keyid:  the ID of the PGP key.
+        """
+        return None #TODO: fix once gpgapi.py is implemented
+
+    def sign_attribute(self, attributeID, expiry):
+        """Send a transaction to sign an attribute.
+
+        attributeID:    the ID of the attribute.
+        expiry:         the expiry time of the attribute in unix time.
+        """
+
+        args = [attributeID, expiry]
+        data = self._contracttranslator.encode('signAttribute', args)
+        return self._send_transaction(data)
+
+    def revoke_signature(self, signatureID):
+        """Sends a transaction to revoke a signature.
+
+        signatureID:    the ID of the signature.
+        """
+
+        args = [signatureID]
+        data = self._contracttranslator.encode('revokeSignature', args)
+        return self._send_transaction(data)
